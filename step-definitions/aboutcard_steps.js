35f51dae1d8af4bdc7046ea87694f20fc6940293
dunjabb <dunjicaboss@gmail.com>
2022-07-26 18:42:14 +0200

    update

--- step-definitions/aboutcard_steps.js
+++ step-definitions/aboutcard_steps.js
@@ -2,23 +2,6 @@ const { Given, When, Then } = require('@wdio/cucumber-framework');
 const LoginPage = require('../support/pages/loginPage');
 const HomePage = require('../support/pages/homePage'); 
 
-Given(/^The user is on the Login page of Saucedemo$/, () => {
-	browser.url('/');
-});
-
-When(/^The user types his username and password$/, () => {
-	LoginPage.inputUsernameAndPassword("standard_user", "secret_sauce");
-});
-
-When(/^The user clicks on the login button$/, () => {
-	LoginPage.clickLoginButton();
-});
-
-Then(/^The user will be directed to the Homepage of Saucedemo Swag Labs$/, () => {
-	browser.pause(1000) 
-	HomePage.checkIfUserIsLogedIn();
-});
-
 Given(/^The user is on the Home page of Saucedemo$/, () => {
 	browser.url('https://www.saucedemo.com/inventory.html');
 });
