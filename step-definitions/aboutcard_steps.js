c4196981d9b3f0357bb77ed620cdc8a26afa80ca
dunjabb <dunjicaboss@gmail.com>
2022-07-26 16:59:20 +0200

    test1

--- step-definitions/aboutcard_steps.js
+++ step-definitions/aboutcard_steps.js
@@ -3,7 +3,7 @@ const LoginPage = require('../support/pages/loginPage');
 const HomePage = require('../support/pages/homePage'); 
 
 Given(/^The user is on the Login page of Saucedemo$/, () => {
-	browser.url("/");
+	browser.url('/');
 });
 
 When(/^The user types his username and password$/, () => {
@@ -17,4 +17,20 @@ When(/^The user clicks on the login button$/, () => {
 Then(/^The user will be directed to the Homepage of Saucedemo Swag Labs$/, () => {
 	browser.pause(1000) 
 	HomePage.checkIfUserIsLogedIn();
+});
+
+Given(/^The user is on the Home page of Saucedemo$/, () => {
+	browser.url('https://www.saucedemo.com/inventory.html');
+});
+
+When(/^The user clicks on burger menu button$/, () => {
+	HomePage.clickBurgerMenuButton();
+});
+
+When(/^The user clicks on the about button$/, () => {
+	HomePage.clickAboutCard();
+});
+
+Then(/^The user will be directed to the Saucelabs website$/, () => {
+	browser.url('https://saucelabs.com/');
 });
