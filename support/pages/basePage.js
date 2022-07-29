cd94d68728b1ed2f8e20edf25f58e255f1926bfe
dunjabb <dunjicaboss@gmail.com>
2022-07-29 19:39:10 +0200

    update

--- support/pages/basePage.js
+++ support/pages/basePage.js
@@ -2,5 +2,22 @@
 * main page object containing all methods, selectors and functionality
 * that is shared across all page objects
 */
-module.exports = class Page {
+module.exports = class BasePage {
+
+    getText(element) {
+        element.waitForDisplayed({ timeout: 30000 })
+        return element.getText()
+    }
+
+    click(element) {
+        element.waitForDisplayed({ timeout: 30000 })
+        element.click()
+    }
+
+    setValue(element, value) {
+        element.waitForDisplayed({ timeout: 30000 })
+        element.setValue(value)
+    }
+
 }
+
